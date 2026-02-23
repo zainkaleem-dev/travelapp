d0e41b5d197a2674a2341a9d26c09a812bd90d98
zainkaleem-dev <zainkaleem@quadtrum.com>
2026-02-23 14:42:19 +0500

    project

---
+++ resources/views/pages/home/⚡create.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    {{-- Do what you can, with what you have, where you are. - Theodore Roosevelt --}}
+</div>
