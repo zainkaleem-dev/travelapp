57b33232f48d426c6b6f3667595254ce1add83a9
Muhammad-Saaim111 <saaim@quadtrum.com>
2026-02-25 23:56:50 +0500

    layout set at all search flow pages

--- resources/views/Livewire/pages/booking/booking-confirmation-success.blade.php
+++ resources/views/Livewire/pages/booking/booking-confirmation-success.blade.php
@@ -1,172 +1,7 @@
-﻿<div>
+<div>
 
-    <!-- preloader area -->
-    <div class="preloader">
-        <div class="d-table">
-            <div class="d-table-cell">
-                <div class="load-spinner">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="pagewrap modify-search">
         <div class="head-wrapper">
-            <!-- page header section -->
-            <header class="header header-light">
-                <div class="container">
-                    <div class="sideMenu">
-                        <button type="button" class="btn rounded-0 btn-left toggle-slide-right"
-                            data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop"
-                            aria-controls="offcanvasWithBackdrop">
-                            <i class="bi bi-text-center"></i>
-                        </button>
-                        <div class="offcanvas offcanvas-start theme-bg-secondary border-0" tabindex="-1"
-                            id="offcanvasWithBackdrop">
-                            <div class="p-0">
-                                <span class="p-0" id="offcanvasLabel"></span>
-                                <button type="button" class="btn-close custom-btn-close theme-text-white"
-                                    data-bs-dismiss="offcanvas" aria-label="Close">
-                                    <i class="bi bi bi-x-lg"></i>
-                                </button>
-                            </div>
-                            <!-- left side menu section -->
-                            <div class="offcanvas-body p-0 mt-5">
-                                <ul class="menuList nav flex-column w-100">
-                                    <li class="nav-item"><a href="index.html" class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-house-fill ps-4 pe-2"></i>Home
-                                        </a>
-                                    </li>
-                                    <li class="nav-item"><a href="deals-offers.html"
-                                            class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-chat-square-heart-fill ps-4 pe-2"></i>Specials Deals
-                                        </a>
-                                    </li>
-                                    <li class="nav-item"><a href="#travelDeals" class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-bookmark-check-fill ps-4 pe-2"></i>Travel Deals
-                                        </a>
-                                    </li>
-                                    <li class="nav-item"><a href="#featuredFlights"
-                                            class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-bell-fill ps-4 pe-2"></i>Featured Flights
-                                        </a>
-                                    </li>
-                                    <li class="nav-item"><a href="#howWork" class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-shield-fill-exclamation ps-4 pe-2"></i>How it Work
-                                        </a>
-                                    </li>
-                                    <li class="nav-item"><a href="about.html" class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-balloon-fill ps-4 pe-2"></i>About Us
-                                        </a>
-                                    </li>
-                                    <li class="nav-item"><a href="contact.html" class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-arrows-move ps-4 pe-2"></i>Contact Us
-                                        </a>
-                                    </li>
-                                    <li class="nav-item"><a href="blog.html" class="position-relative card-effect">
-                                            <div class="card-box"></div>
-                                            <i class="bi bi-arrow-right-circle-fill ps-4 pe-2"></i>Blogs
-                                        </a>
-                                    </li>
-                                </ul>
-                                <div class="mt-5">
-                                    <p class="d-inline-flex theme-text-white">
-                                        <i class="bi bi-telephone-fill h4 theme-text-white ps-4 pe-2"></i>
-                                        24Ã—7 Customer Support <br> 1800-313-5699
-                                    </p>
-                                </div>
-                                <div class="mt-3">
-                                    <p class="d-inline-flex theme-text-white">
-                                        <i class="bi bi-envelope-fill h4 theme-text-white ps-4 pe-2"></i>
-                                        Email us <br> support@example.com
-                                    </p>
-                                </div>
-                                <p class="ps-4 mb-0 mt-5 theme-text-white">Social Network</p>
-                                <div class="d-flex social mt-3 ps-4">
-                                    <a href="#" class="h4 pe-3"><i class="bi bi-facebook"></i></a>
-                                    <a href="#" class="h4 px-3"><i class="bi bi-twitter-x"></i></a>
-                                    <a href="#" class="h4 px-3"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#" class="h4 px-3"><i class="bi bi-instagram"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <nav class="navbar navbar-expand-md navbar-light py-0 px-0">
-                        <a class="navbar-brand ms-5" href="index.html"><img src="assets/images/logo.png"
-                                alt="Brand Logo" title="Brand Logo" class="img-fluid"></a>
-                        <button class="navbar-toggler px-1 btn rounded-0" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ms-auto">
-                                <li class="nav-item">
-                                    <div class="dropdown-container pe-md-4">
-                                        <div class="dropdown-toggle click-dropdown">
-                                            <i class="bi bi-currency-exchange"></i> Currency
-                                        </div>
-                                        <div class="dropdown-menu">
-                                            <ul>
-                                                <li class="nav-item"><a class="dropdown-item" href="#">INR</a></li>
-                                                <li class="nav-item"><a class="dropdown-item" href="#">USD</a></li>
-                                                <li class="nav-item"><a class="dropdown-item" href="#">EUR</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <div class="dropdown-container pe-md-4">
-                                        <div class="dropdown-toggle click-dropdown">
-                                            <i class="bi bi-translate"></i> Language
-                                        </div>
-                                        <div class="dropdown-menu">
-                                            <ul>
-                                                <li class="nav-item"><a class="dropdown-item" href="#">English</a></li>
-                                                <li class="nav-item"><a class="dropdown-item" href="#">Rusian</a></li>
-                                                <li class="nav-item"><a class="dropdown-item" href="#">French</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <div class="dropdown-container">
-                                        <div class="dropdown-toggle click-dropdown">
-                                            <i class="bi bi-person-circle"></i> Account
-                                        </div>
-                                        <div class="dropdown-menu">
-                                            <ul>
-                                                <li class="nav-item"><a class="dropdown-item"
-                                                        href="signin.html">Login</a></li>
-                                                <li class="nav-item"><a class="dropdown-item"
-                                                        href="signup.html">Register</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-            </header>
             <!-- review booking flight-->
             <div class="flight-search">
                 <div class="container">
@@ -358,7 +193,7 @@
                                                 <div class="collapse" id="baseFare">
                                                     <div class="d-flex justify-content-between pt-3">
                                                         <span class="font-medium">Adult(s)
-                                                            (1 X â‚¹ 11,545)</span>
+                                                            (1 X ₹ 11,545)</span>
                                                         <span class="font-medium">
                                                             <i class="bi bi-currency-dollar"></i>
                                                             <span class="fw-normal">11,545</span>
@@ -380,7 +215,7 @@
                                                 <div class="collapse" id="airlineSurcharge">
                                                     <div class="d-flex justify-content-between pt-3">
                                                         <span class="font-medium">Adult(s)
-                                                            (1 X â‚¹ 8,0)</span>
+                                                            (1 X ₹ 8,0)</span>
                                                         <span class="font-medium">
                                                             <i class="bi bi-currency-dollar"></i>
                                                             <span class="fw-normal">800</span>
@@ -476,113 +311,7 @@
 
 
         </div>
-        <!-- page footer section -->
-        <footer class="footer" id="footerSec">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-lg-6 pt-5">
-                        <img src="assets/images/logo.png" class="img-fluid" alt="Brand color white"
-                            title="Brand color white">
-                        <p class="text-justify pt-5">Flight World Travel Agent dashboard is a specialized travel service
-                            for organizations from flightworld.com</p>
-                        <p class="pt-lg-5">Get Latest Deals, Upcoming Flight Offers and Cheap Fare</p>
-                        <form class="form">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control py-3 rounded-0" id="inputPassword2"
-                                            placeholder="Enter your email address">
-                                        <button type="button"
-                                            class="btn btn-outline-light rounded-0 custom-btn-subscribe btn-effect">Subscribe</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                        <div class="col-12 mt-5">
-                            <img src="assets/images/icons/tafi.svg" class="img-fluid me-4" alt="tafi" title="tafi">
-                            <img src="assets/images/icons/taai.svg" class="img-fluid" alt="taai" title="taai">
-                        </div>
-                    </div>
-                    <div class="col-12 col-lg-6 pt-5">
-                        <img src="assets/images/icons/iata.svg" class="img-fluid d-inline-flex" alt="IATA" title="IATA">
-                        <p class="fw-bold text-uppercase mb-0 ms-2 d-inline-flex">We are an IATA Certified travel agency
-                        </p>
-                        <div class="row">
-                            <div class="col-md-4 mt-5">
-                                <p class="text-uppercase fw-bold mb-4">About Us</p>
-                                <ul class="fl-menu">
-                                    <li class="nav-item"><a href="about.html">About</a></li>
-                                    <li class="nav-item"><a href="contact.html">Contact us</a></li>
-                                    <li class="nav-item"><a href="#">Bank Details</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-md-4 mt-5">
-                                <p class="text-uppercase fw-bold">Legal</p>
-                                <ul class="fl-menu">
-                                    <li class="nav-item"><a href="privacy.html">Privacy</a></li>
-                                    <li class="nav-item"><a href="#">T&C</a></li>
-                                    <li class="nav-item"><a href="#">Disclaimer</a></li>
-                                    <li class="nav-item"><a href="#">Privacy and Cookies</a></li>
-                                    <li class="nav-item"><a href="#">Legal</a></li>
-                                    <li class="nav-item"><a href="#">Help</a></li>
-                                    <li class="nav-item"><a href="#">Feedback</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-md-4 mt-5">
-                                <p class="text-uppercase fw-bold">Company</p>
-                                <ul class="fl-menu">
-                                    <li class="nav-item"><a href="#">Partner With Us</a></li>
-                                    <li class="nav-item"><a href="#">Services</a></li>
-                                    <li class="nav-item"><a href="#">Careers</a></li>
-                                    <li class="nav-item"><a href="#">Products</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row g-0 pt-5">
-                    <div class="col-12 border-top border-bottom">
-                        <div class="row">
-                            <div class="col-12 col-lg-4 text-start py-3">
-                                <p class="d-inline-flex text-uppercase mb-0">Follow Us</p>
-                                <div class="d-inline-flex social">
-                                    <a href="#" class="ps-3"><i class="bi bi-facebook"></i></a>
-                                    <a href="#" class="ps-3"><i class="bi bi-twitter-x"></i></a>
-                                    <a href="#" class="ps-3"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#" class="ps-3"><i class="bi bi-instagram"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-4 text-lg-center social py-3">
-                                <p class="d-inline-flex text-uppercase mb-0">Support Center:</p>
-                                <a href="tel:+1234567890" class="botom-link">(769) 25698745</a>
-                            </div>
-                            <div class="col-12 col-lg-4 text-lg-end social py-3">
-                                <a href="mailto:support@example.com" class="botom-link">support@example.com</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <p class="text-center py-5 mb-0 font-small">Â©2025 Flight World Pwt Lcd. All Rights Reserved.
-                            Various trademarks are held by their respective owners.
-                        </p>
-                    </div>
-                </div>
-
-            </div>
-            <p id="back-top" class="back-to-top bg-dark" style="display: block;">
-                <a href="#top"><i class="bi bi-chevron-up"></i></a>
-            </p>
-        </footer>
-
-    </div>
-
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <!--This page JavaScript -->
-    <!--Data Calendar js -->
-    <!--Traveler counter js -->
-    <!-- animation aos library -->
-    
+</div>
 </div>
 
 
