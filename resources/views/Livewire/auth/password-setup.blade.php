9f1116f4073dc016679cc584840a6769ce6999a2
Mujtaba <03mujtabaasad@gmail.com>
2026-04-28 15:56:21 +0500

    Added eyes to password feilds

--- resources/views/Livewire/auth/password-setup.blade.php
+++ resources/views/Livewire/auth/password-setup.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data="{ showNewPassword: false, showConfirmPassword: false }">
     <div class="flex-1 flex flex-col min-h-screen items-start sm:items-center justify-center px-4 py-8">
         <div class="flex justify-center">
             <img src="{{ asset('assets/images/travelapp_logo.png') }}" alt="Logo" class="h-64 w-auto">
@@ -25,8 +25,24 @@
                                             d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                                     </svg>
                                 </div>
-                                <input id="new_password" type="password" wire:model.defer="new_password" placeholder="••••••••"
-                                    class="input-field @error('new_password') border-red-400 bg-red-50 @enderror">
+                                <input id="new_password" :type="showNewPassword ? 'text' : 'password'" wire:model.defer="new_password" placeholder="••••••••"
+                                    class="input-field pr-10 @error('new_password') border-red-400 bg-red-50 @enderror">
+                                <button type="button" @click="showNewPassword = !showNewPassword"
+                                    class="absolute inset-y-0 right-3 inline-flex items-center text-gray-400 hover:text-gray-600"
+                                    :title="showNewPassword ? 'Hide password' : 'Show password'">
+                                    <svg x-show="!showNewPassword" x-cloak class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                    <svg x-show="showNewPassword" x-cloak class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.956 9.956 0 012.042-3.368M9.88 9.88a3 3 0 104.24 4.24" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M6.1 6.1A9.958 9.958 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.964 9.964 0 01-4.132 5.411M3 3l18 18" />
+                                    </svg>
+                                </button>
                             </div>
                             @error('new_password')
                                 <p class="mt-1 text-red-500" style="font-size:10px">{{ $message }}</p>
@@ -44,8 +60,24 @@
                                             d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                                     </svg>
                                 </div>
-                                <input id="new_password_confirmation" type="password" wire:model.defer="new_password_confirmation" placeholder="••••••••"
-                                    class="input-field @error('new_password_confirmation') border-red-400 bg-red-50 @enderror">
+                                <input id="new_password_confirmation" :type="showConfirmPassword ? 'text' : 'password'" wire:model.defer="new_password_confirmation" placeholder="••••••••"
+                                    class="input-field pr-10 @error('new_password_confirmation') border-red-400 bg-red-50 @enderror">
+                                <button type="button" @click="showConfirmPassword = !showConfirmPassword"
+                                    class="absolute inset-y-0 right-3 inline-flex items-center text-gray-400 hover:text-gray-600"
+                                    :title="showConfirmPassword ? 'Hide password' : 'Show password'">
+                                    <svg x-show="!showConfirmPassword" x-cloak class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                    <svg x-show="showConfirmPassword" x-cloak class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.956 9.956 0 012.042-3.368M9.88 9.88a3 3 0 104.24 4.24" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M6.1 6.1A9.958 9.958 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.964 9.964 0 01-4.132 5.411M3 3l18 18" />
+                                    </svg>
+                                </button>
                             </div>
                             @error('new_password_confirmation')
                                 <p class="mt-1 text-red-500" style="font-size:10px">{{ $message }}</p>
--- resources/views/Livewire/user/create.blade.php
+++ resources/views/Livewire/user/create.blade.php
@@ -138,9 +138,28 @@
                             </p> @enderror
                         </div>
 
-                        <div>
+                        <div x-data="{ showPassword: false }">
                             <label class="field-label">Password <span class="text-red-500">*</span></label>
-                            <input type="password" wire:model="password" class="input-field" placeholder="••••••••">
+                            <div class="relative">
+                                <input :type="showPassword ? 'text' : 'password'" wire:model="password" class="input-field pr-10"
+                                    placeholder="••••••••">
+                                <button type="button" @click="showPassword = !showPassword"
+                                    class="absolute inset-y-0 right-3 inline-flex items-center text-gray-400 hover:text-gray-600"
+                                    :title="showPassword ? 'Hide password' : 'Show password'">
+                                    <svg x-show="!showPassword" x-cloak class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                    <svg x-show="showPassword" x-cloak class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.956 9.956 0 012.042-3.368M9.88 9.88a3 3 0 104.24 4.24" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M6.1 6.1A9.958 9.958 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.964 9.964 0 01-4.132 5.411M3 3l18 18" />
+                                    </svg>
+                                </button>
+                            </div>
                             @error('password') <p class="mt-1 text-[11px] font-bold text-red-500 uppercase">
                                 {{ $message }}
                             </p> @enderror
